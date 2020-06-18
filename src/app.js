c9a4e533370da3713ffc739d998d4867e7551f04
João Eliandro <eliandrogermano@gmail.com>
2020-06-18 02:30:40 -0300

    Add delete method repository

--- src/app.js
+++ src/app.js
@@ -50,6 +50,15 @@ app.put("/repositories/:id", (request, response) => {
 
 app.delete("/repositories/:id", (request, response) => {
   // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) return response.status(400).json({ error: "Not found!" });
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.patch("/repositories/:id/like", (request, response) => {
